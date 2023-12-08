0bf854b0a7e09c9516e6dbf751751f4f1b46f90f
TamHM <hoangminhtam1602@gmail.com>
2023-12-09 00:00:10 +0700

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Resources\UserResource;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\FilmController;
 
 
-Route::get('promotions/findByCode/{code}', [PromotionController::class, 'findByCode']);
 Route::group(['prefix' => 'users'], function () {
-    Route::post('users/{id}', [UserController::class, 'update']);
-    Route::get('users/{id}', [UserController::class, 'show']);
-    Route::post('users', [UserController::class, 'store']);
-    Route::get('users', [UserController::class, 'index']);
+    Route::post('/{id}', [UserController::class, 'update']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/', [UserController::class, 'index']);
+});
+Route::group(['prefix' => 'promotions'], function () {
+    Route::get('/findByCode/{code}', [PromotionController::class, 'findByCode']);
+    Route::get('/{id}', [PromotionController::class, 'show']);
+    Route::get('/', [PromotionController::class, 'index']);
+});
+
+Route::group(['prefix' => 'films'], function () {
+    Route::get('/{id}', [FilmController::class, 'show']);
+    Route::get('/', [FilmController::class, 'index']);
 });
